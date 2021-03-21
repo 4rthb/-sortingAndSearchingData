a71e86f14c13f5b702723f99321ada1a993005c6
spopsin <spopsin@idejr.com.br>
2021-03-21 18:53:47 -0300

    começo do programa

---
+++ lab2/QuickSort.py
@@ -0,0 +1,7 @@
+entrada = open("entrada.txt","r")
+n = entrada.readline()      # n = numero de casos
+while n
+    m = entrada.readline(1)     # m = numero de elementos do array
+    array = entrada.read()
+    print(array)
+    
