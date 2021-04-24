e3cfd3c7584b8aedd7d7ca876b6f5b168b7714de
Arthur Prochnow Baumgardt <b_arthp@outlook.com>
2021-04-23 21:16:13 -0300

    file and function handling

--- tf/main.py
+++ tf/main.py
@@ -2,12 +2,39 @@ import hash
 import trie
 import csv
 import time
+import re
 
-file = "rating.csv"
+files = ["minirating.csv", "movie_clean.csv", "tag_clean.csv"]
+buffer = []
+pattern = [re.compile('user (\d*)$'), re.compile('movie ([^\n]*)$'), re.compile('tags ([^\n]*)$'), re.compile('top(\d*) ([^\n]*)$')]
+
+for i in range(len(files)):
+    buffer.append([])
+
 t0 = time.process_time()
-with open(file, newline='') as csvfile:
-    spamreader = csv.reader(csvfile)
-    for row in spamreader:
-        # print(row)
-        pass
-print(time.process_time()-t0)
+
+for file in files:
+    with open(file, newline='') as csvfile:
+        buffer[files.index(file)] = list(csv.reader(csvfile))
+        buffer[files.index(file)].remove(buffer[files.index(file)][0])
+
+while True:
+    inp = input("What do you want to do?\n(X) - Exit\n")
+
+    if inp == "X" or inp == "x":
+        break
+    elif pattern[0].search(inp): #user ratings
+        print(pattern[0].search(inp).group(1))
+    elif pattern[1].search(inp): #movie search
+        print(pattern[1].search(inp).group(1))
+    elif pattern[2].search(inp): #tag search
+        tags = pattern[2].search(inp).group(1)
+        tags = tags.split(" ")
+        tags[:] = [ tag.replace("'", "") for tag in tags ]
+        print(tags)
+    elif pattern[3].search(inp): #top nth from genre
+        n = int(pattern[3].search(inp).group(1))
+        genre = eval(pattern[3].search(inp).group(2))
+    else:
+        print("Invalid command, try again.")
+print("Goodbye! :)")
