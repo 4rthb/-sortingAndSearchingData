ecec10a5c4ee38c41b3bf25afe1b0d69e94cd564
Arthur Prochnow Baumgardt <b_arthp@outlook.com>
2021-05-03 17:03:56 -0300

    User hash table implemented

--- tf/hash.py
+++ tf/hash.py
@@ -2,23 +2,51 @@ def hashingUM(n, size):
     return int(n % size)
 
 def hashingT(tag,size):
-
+    i = 0
+    hash = 0
+    tag = tag.replace(" ", "")
+    while i < len(tag):
+        hash = (ord(tag[i]) + 127 * hash) % size
+        i += 1
+    return int(hash)
 
 def insertChainUT(hash, tuple, table):
 
+    tag = 0
     for node in table:
         if node[0] == hash:
+            oldnode = node
             node=node[:-1]
-            node.append([tuple])
+            for subnode in node[1:]:
+                if subnode[0] == tuple[0]:
+                    node[node.index(subnode)].append(tuple[1])
+                    node[node.index(subnode)].append(tuple[2])
+                    tag = 1
+            if not tag:
+                node.append(tuple)
             node.append('\0')
+            table[table.index(oldnode)]=node
             break
 
 def insertChainM(hash, tuple, table):
+
 # tuple = [movieid, name, genre, avg rating, total user ratings]
+# or
+# tuple = [movieid, 0, 0, rating, 0](pra n precisar acessar toda hora o negocio das outras coisas)
+
     for node in table:
         if node[0] == hash:
             node=node[:-1]
-            node.append([tuple])
+            if len(node[1:])!=0:
+                for movie in node[1:]:
+                    if movie[0]==tuple[0]:
+                        if movie[1]==0:
+                            movie[1]=tuple[1]
+                            movie[2]=tuple[2]
+                        movie[3]=(movie[3]*movie[4]+tuple[1])/(movie[4]+1)
+                        movie[4]+=1
+            else:
+                node.append([tuple])
             node.append('\0')
             break
 
